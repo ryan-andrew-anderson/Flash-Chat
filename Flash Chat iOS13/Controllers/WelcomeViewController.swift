dd8aee534bd8faabc0d13695674b8f4b717e3bba
Ryan A. Anderson <ryan.anderson9921@stu.mtec.edu>
2022-01-05 21:56:08 -0700

    Implementing new pod

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -7,27 +7,33 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
     
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        titleLabel.text = ""
-        var charIndex = 0.0
-        let welcomeTitle = "⚡️FlashChat"
-        for letter in welcomeTitle {
-            print("--")
-            print(0.1 * charIndex)
-            print(letter)
-        Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
-            
-                self.titleLabel.text?.append(letter)
-            }
-            charIndex += 1
-        }
+        titleLabel.text = "⚡️FlashChat"
+        
+//        titleLabel.text = ""
+//        var charIndex = 0.0
+//        let welcomeTitle = "⚡️FlashChat"
+//        for letter in welcomeTitle {
+//            print("--")
+//            print(0.1 * charIndex)
+//            print(letter)
+//        Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
+//
+//                self.titleLabel.text?.append(letter)
+//            }
+//            charIndex += 1
+//        }
+        
+        
+        
     }
 }
--- Podfile
+++ Podfile
@@ -3,7 +3,6 @@
 target 'Flash Chat iOS13' do
 
   # Pods for Flash Chat iOS13
- 
  pod 'CLTypingLabel','~> 0.4.0'
-
+ 
 end
