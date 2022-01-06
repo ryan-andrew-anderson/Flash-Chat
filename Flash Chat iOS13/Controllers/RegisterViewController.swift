b434d037a2ee618c4ddf84bcfa3481dfc5affd21
Ryan A. Anderson <ryan.anderson9921@stu.mtec.edu>
2022-01-06 09:00:33 -0700

    Firebase Auth.createUser enabling

--- Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -7,13 +7,25 @@
 //
 
 import UIKit
+import Firebase
+import CoreMedia
 
-class RegisterViewController: UIViewController {
-
+class  RegisterViewController: UIViewController {
+    
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
     
     @IBAction func registerPressed(_ sender: UIButton) {
+        
+        if let email =  emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let err = error {
+                    print(err.localizedDescription )
+                } else {
+                    // Navigate to the chatViewController
+                    self.performSegue(withIdentifier: "RegisterToChat", sender: self)
+                }
+            }
+        }
     }
-    
 }
